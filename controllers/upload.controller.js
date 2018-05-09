b104e59cc807c230129cbf379e06c66fd22662ad
nikhil8968 <nikhil.taralekar@dahlia-tech.com>
2018-05-09 18:59:57 +0530

    file upload

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,70 +1,76 @@
-let express = require('express'),
+let express = require('express'), 
     fs      = require('fs'),
-    fse     = require('fs-extra'),
+    aws = require('aws-sdk'),
+    multerS3 = require('multer-s3'),
     multer  = require('multer'),
-    mime    = require('mime'),
-    path    = require('path'),
     crypto  = require('crypto'),
-    mkdirP  = require('mkdirp'),
-    config  = require('../config/config'),
-    gm      = require('gm').subClass({imageMagick: true});
+    config  = require('../config/config');
+    require('dotenv').load()
 
 
 process.on('uncaughtException', function (err) {
   console.log(err);
 });
 
-let rmDir = (dirPath, removeSelf) => {
-  if (removeSelf === undefined)
-    removeSelf = true;
-  try {
-    var files = fs.readdirSync(dirPath);
-  }
-  catch (e) {
-    return;
-  }
-  if (files.length > 0)
-    for (let i = 0; i < files.length; i++) {
-      let filePath = dirPath + '/' + files[i];
-      if (fs.statSync(filePath).isFile())
-        fs.unlinkSync(filePath);
-      else
-        rmDir(filePath);
-    }
-  if (removeSelf)
-    fs.rmdirSync(dirPath);
-};
+aws.config.update({
+  secretAccessKey: process.env.secretAccessKey,
+  accessKeyId: process.env.accessKeyId
+});
+s3 = new aws.S3();
 
 // create a Temp storage for images, when the user submits the form, the temp image will be moved to the appropriate folder inside /uploads/forms/user_id/photo.jpg
-let tempStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    let dest = config.paths.tmpImagePath;
-    let stat = null;
-    try {
-      stat = fs.statSync(dest);
-    }
-    catch (err) {
-      fs.mkdirSync(dest);
-    }
-    if (stat && !stat.isDirectory()) {
-      throw new Error('Directory cannot be created because an inode of a different type exists at "' + dest + '"');
-    }
-    cb(null, dest);
-  },
-  filename   : (req, file, cb) => {
-    // if you want even more random characters prefixing the filename then change the value '2' below as you wish, right now, 4 charaters are being prefixed
-    crypto.pseudoRandomBytes(4, (err, raw) => {
-      let filename = file.originalname.replace(/_/gi, '');
-      cb(null, raw.toString('hex') + '.' + filename.toLowerCase());
-    });
+let tempStorage=multerS3({
+  s3: s3,
+  bucket: process.env.bucketName,
+  acl: 'public-read',
+  key: function (req, file, cb) {
+        crypto.pseudoRandomBytes(4, (err, raw) => {
+          let filename = file.originalname.replace(/_/gi, '');
+           cb(null, config.aws.tmpImagePath + raw.toString('hex') + '.' + filename.toLowerCase());
+        });
   }
 });
 
-//  multer configuration
+// When user submits the form, the tmp image is copied to /dest/photo.jpg
+let copyImage = (profileImage,dest) => {
+   
+        var key = profileImage.replace('tmp/','');
+       
+        var params = {
+            Bucket : process.env.bucketName, 
+            CopySource : '/' + process.env.bucketName +'/' + config.aws.tmpImagePath + key, 
+            Key : dest + key,
+            ACL : 'public-read',
+        };
+        s3.copyObject(params, function(err, data) {
+            if (err)
+                console.log(err); // an error occurred
+            else {
+              deleteImage(profileImage);
+              
+            }
+        });
+};
+
+
+//  delete Image
+let deleteImage = (imagekey) =>{
+  const params = {
+    Bucket: process.env.bucketName,
+     Key: imagekey
+  };
+  s3.deleteObject(params, function (err, data) {
+    if (err) 
+       console.log(err);
+    });
+
+};
+
+//  upload document Image
 let documentsTemp = multer({
   storage   : tempStorage,
   limits    : {
-    fileSize: 5000000, // 5MB filesize limit
+    fileSize: config.aws.fileSize, // 5MB filesize limit
     parts   : 1
   },
   fileFilter: (req, file, cb) => {
@@ -78,11 +84,12 @@ let documentsTemp = multer({
   }
 }).single('documents');
 
-//  multer configuration
+
+//  upload profile Image
 let profileTemp = multer({
     storage   : tempStorage,
     limits    : {
-      fileSize: 5000000, // 5MB filesize limit
+      fileSize: config.aws.fileSize, // 5MB filesize limit
       parts   : 1
     },
     fileFilter: (req, file, cb) => {
@@ -96,85 +103,58 @@ let profileTemp = multer({
     }
 }).single('profile');
 
-// When user submits the form, the temp image is copied to /uploads/forms/user_id/photo.jpg
-let copyImage = (req, source) => {
-  mkdirP(config.paths.imagePath + req.user._id, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    fse.copy(config.paths.tmpImagePath + source, config.paths.imagePath + req.user._id + '/' + source)
-      .then(() => {
-      })
-      .catch((error) => console.log(error));
-  });
-};
-
-let deleteImage = (image) => {
-  fse.remove(config.paths.tmpImagePath + image)
-    .then(() => {
-      console.log('success!');
-    })
-    .catch(err => {
-      console.error(err);
-    });
-};
-
 let functions = {
-
-  // Upload Image to Server Temp Folder
-  uploadDocumentImage: (req, res) => {
-    documentsTemp(req, res, (err) => {
-      if (err) {
-        console.log(err);
-      }
-      if (req.file !== undefined) {
-        gm(req.file.path)
-          .resize(445, null)
-          .noProfile()
-          .write(req.file.path, (err) => {
-            if (err) {
-              console.log(err);
-              res.status(500).json({
-                message: 'The file you selected is not an image'
-              });
-            }
-            res.status(200).json({"imageName":req.file.filename});
-          });
-      }
-    });
-  },
-
-  // // Delete Temporary Image From Temp Folder
+  //delete image from S3
   deleteImage: (req, res) => {
-    let params = req.params.id;
-    deleteImage(params);
-    res.status(200).json({
-      message: 'Image deleted successfully!'
-    });
+       let params = req.body.key;
+       deleteImage(params);
+      res.status(200).json({
+        message: 'Image deleted successfully!'
+       });
   },
 
-  // Upload Image to Server Temp Folder
+  // Upload profile Image to S3 tmp Folder
   uploadProfile: (req, res) => {
     profileTemp(req, res, (err) => {
       if (err) {
-        console.log(err);
+        return res.status(500).json(err);
       }
       if (req.file !== undefined) {
-        gm(req.file.path)
-          .resize(445, null)
-          .noProfile()
-          .write(req.file.path, (err) => {
-            if (err) {
-              console.log(err);
-              res.status(500).json({
-                message: 'The file you selected is not an image'
-              });
-            }
-            res.status(201).json(req.file.filename);
-          });
+        res.status(200).json({
+          message: 'Profile Image uploaded successfully!',key:req.file.key
+        });
       }
     });
   },
+
+  // Upload document Image to S3 tmp Folder
+  uploadDocument: (req, res) => {
+    documentsTemp(req, res, (err) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      if (req.file !== undefined) {
+        res.status(200).json({
+          message: 'Document Image uploaded successfully!',key:req.file.key
+        });
+      }
+    });
+  },
+
+  //upload Image to S3 dest Folder
+  uploadImage: (req,res) => 
+  {
+    let imageKey = req.body.key;
+    let destFolder = req.body.dest;
+
+    copyImage(imageKey,destFolder);
+    var key = imageKey.replace('tmp/','');
+    res.status(200).json({
+      message: 'Image save successfully!',key:destFolder + key
+     });
+    
+
+  }
 };
 
 module.exports = functions;
