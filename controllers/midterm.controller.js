85f27cccfe91530a3043b246e6b9462509b36b78
ODIN-PC <bishtamith@gmail.com>
2018-12-09 01:10:20 +0530

    mtr batch changes

--- controllers/midterm.controller.js
+++ controllers/midterm.controller.js
@@ -6,115 +6,116 @@ let KraWorkFlowInfo = require("../models/kra/kraWorkFlowDetails.model"),
 function EmpDetailsForMidTermInitiate(req, res) {
   KraWorkFlowInfo.aggregate([
     {
-      "$project": {
-        "emp_id": "$emp_id",
-        "batch_id": "$batch_id",
-        "status": "$status"
+      $project: {
+        emp_id: "$emp_id",
+        batch_id: "$batch_id",
+        status: "$status"
       }
     },
     {
-      "$lookup": {
-        "from": "employeedetails",
-        "localField": "emp_id",
-        "foreignField": "_id",
-        "as": "employee_details"
+      $lookup: {
+        from: "employeedetails",
+        localField: "emp_id",
+        foreignField: "_id",
+        as: "employee_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$employee_details"
+      $unwind: {
+        path: "$employee_details"
       }
     },
     {
-      "$lookup": {
-        "from": "employeeofficedetails",
-        "localField": "emp_id",
-        "foreignField": "emp_id",
-        "as": "employee_office_details"
+      $lookup: {
+        from: "employeeofficedetails",
+        localField: "emp_id",
+        foreignField: "emp_id",
+        as: "employee_office_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$employee_office_details"
+      $unwind: {
+        path: "$employee_office_details"
       }
     },
     {
-      "$lookup": {
-        "from": "midtermmasters",
-        "localField": "emp_id",
-        "foreignField": "emp_id",
-        "as": "mtr_master_details"
+      $lookup: {
+        from: "midtermmasters",
+        localField: "emp_id",
+        foreignField: "emp_id",
+        as: "mtr_master_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$mtr_master_details",
-        "preserveNullAndEmptyArrays": true
+      $unwind: {
+        path: "$mtr_master_details",
+        preserveNullAndEmptyArrays: true
       }
     },
     {
-      "$lookup": {
-        "from": "designations",
-        "localField": "employee_details.designation_id",
-        "foreignField": "_id",
-        "as": "designation_details"
+      $lookup: {
+        from: "designations",
+        localField: "employee_details.designation_id",
+        foreignField: "_id",
+        as: "designation_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$designation_details"
+      $unwind: {
+        path: "$designation_details"
       }
     },
     {
-      "$lookup": {
-        "from": "employeesupervisordetails",
-        "localField": "emp_id",
-        "foreignField": "emp_id",
-        "as": "employee_supervisor_details"
+      $lookup: {
+        from: "employeesupervisordetails",
+        localField: "emp_id",
+        foreignField: "emp_id",
+        as: "employee_supervisor_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$employee_supervisor_details"
+      $unwind: {
+        path: "$employee_supervisor_details"
       }
     },
     {
-      "$lookup": {
-        "from": "employeedetails",
-        "localField": "employee_supervisor_details.primarySupervisorEmp_id",
-        "foreignField": "_id",
-        "as": "supervisor_details"
+      $lookup: {
+        from: "employeedetails",
+        localField: "employee_supervisor_details.primarySupervisorEmp_id",
+        foreignField: "_id",
+        as: "supervisor_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$supervisor_details"
+      $unwind: {
+        path: "$supervisor_details"
       }
     },
     {
-      "$project": {
-        "emp_id": "$emp_id",
-        "kra_batch_id": "$batch_id",
-        "kra_status": "$status",
-        "emp_full_name": "$employee_details.fullName",
-        "emp_grade_id": "$employee_details.grade_id",
-        "emp_isAccountActive": "$employee_details.isAccountActive",
-        "emp_profileImage": "$employee_details.profileImage",
-        "emp_userName": "$employee_details.userName",
-        "emp_employmentType_id": "$employee_details.employmentType_id",
-        "emp_isDeleted": "$employee_details.isDeleted",
-        "emp_department_id": "$employee_office_details.department_id",
-        "emp_HRSpoc_id": "$employee_office_details.htspoc_id",
-        "emp_officeEmail": "$employee_office_details.officeEmail",
-        "emp_designation_id": "$employee_details.designation_id",
-        "emp_designation_name": "$designation_details.designationName",
-        "emp_supervisor_id": "$employee_supervisor_details.primarySupervisorEmp_id",
-        "emp_supervisor_name": "$supervisor_details.fullName",
-        "mtr_status": "$mtr_master_details.status",
-        "mtr_batch_id": "$mtr_master_details.batch_id"
+      $project: {
+        emp_id: "$emp_id",
+        kra_batch_id: "$batch_id",
+        kra_status: "$status",
+        emp_full_name: "$employee_details.fullName",
+        emp_grade_id: "$employee_details.grade_id",
+        emp_isAccountActive: "$employee_details.isAccountActive",
+        emp_profileImage: "$employee_details.profileImage",
+        emp_userName: "$employee_details.userName",
+        emp_employmentType_id: "$employee_details.employmentType_id",
+        emp_isDeleted: "$employee_details.isDeleted",
+        emp_department_id: "$employee_office_details.department_id",
+        emp_HRSpoc_id: "$employee_office_details.htspoc_id",
+        emp_officeEmail: "$employee_office_details.officeEmail",
+        emp_designation_id: "$employee_details.designation_id",
+        emp_designation_name: "$designation_details.designationName",
+        emp_supervisor_id:
+          "$employee_supervisor_details.primarySupervisorEmp_id",
+        emp_supervisor_name: "$supervisor_details.fullName",
+        mtr_status: "$mtr_master_details.status",
+        mtr_batch_id: "$mtr_master_details.batch_id"
       }
     }
-  ]).exec(function (err, response) {
+  ]).exec(function(err, response) {
     if (err) {
       return res.status(403).json({
         title: "There is a problem",
@@ -141,8 +142,8 @@ function InitiateMtrProcess(req, res) {
   MidTermBatchDetails.isDeleted = false;
   MidTermBatchDetails.createdBy = createdBy;
   let emp_id_array = req.body.emp_id_array;
-  MidTermBatchDetails.transac
-  MidTermBatchDetails.save(function (err, midtermbatchresp) {
+  MidTermBatchDetails.transac;
+  MidTermBatchDetails.save(function(err, midtermbatchresp) {
     if (err) {
       return res.status(403).json({
         title: "There was a problem",
@@ -154,7 +155,14 @@ function InitiateMtrProcess(req, res) {
         }
       });
     } else {
-      AuditTrail.auditTrailEntry(0, "MidTermBatchDetails", midtermbatchresp, "user", "MidTermBatchDetails", "ADDED");
+      AuditTrail.auditTrailEntry(
+        0,
+        "MidTermBatchDetails",
+        midtermbatchresp,
+        "user",
+        "MidTermBatchDetails",
+        "ADDED"
+      );
       let batch_id = midtermbatchresp.id;
       Promise.all([
         MidTermMaster.aggregate([
@@ -172,17 +180,23 @@ function InitiateMtrProcess(req, res) {
             $limit: 1.0
           }
         ]).exec()
-      ]).then(function (counts) {
+      ]).then(function(counts) {
         let insertData = [];
         let midtermMaster_id =
           counts[0][0] === undefined ? 1 : counts[0][0]._id;
-        emp_id_array.forEach(function (element, index) {
+        emp_id_array.forEach(function(element, index) {
           insertData.push({
-            batch_id: batch_id, emp_id: element.emp_id, status: "Initiated", _id: midtermMaster_id + (index + 1),
+            batch_id: batch_id,
+            emp_id: element.emp_id,
+            status: "Initiated",
+            _id: midtermMaster_id + (index + 1),
             createdBy: createdBy
           });
         });
-        MidTermMaster.insertMany(insertData, function (err, midTermMasterResult) {
+        MidTermMaster.insertMany(insertData, function(
+          err,
+          midTermMasterResult
+        ) {
           if (err) {
             return res.status(403).json({
               title: "There was a problem",
@@ -194,53 +208,62 @@ function InitiateMtrProcess(req, res) {
               }
             });
           } else {
-            AuditTrail.auditTrailEntry(0, "MidTermMaster", insertData, "user", "MidTermMaster", "ADDED");
+            AuditTrail.auditTrailEntry(
+              0,
+              "MidTermMaster",
+              insertData,
+              "user",
+              "MidTermMaster",
+              "ADDED"
+            );
             let emp_id_collection = [];
             emp_id_array.forEach(element => {
               emp_id_collection.push(element.emp_id);
             });
             Promise.all([
-              KraWorkFlowInfo.aggregate(
-                [
-                  {
-                    "$match": {
-                      "emp_id": {
-                        "$in": emp_id_collection
-                      },
-                      "status": "Initiated"
-                    }
-                  },
-                  {
-                    "$lookup": {
-                      "from": "kradetails",
-                      "localField": "_id",
-                      "foreignField": "kraWorkflow_id",
-                      "as": "kra_details"
-                    }
-                  },
-                  {
-                    "$unwind": {
-                      "path": "$kra_details",
-                      "preserveNullAndEmptyArrays": true
-                    }
-                  },
-                  {
-                    "$project": {
-                      "kra_batch_id": "$batch_id",
-                      "kra_emp_id": "$emp_id",
-                      "kra_status": "$status",
-                      "kra_details_isDeleted": "$kra_details.isDeleted",
-                      "kra_details_sendBackComment": "$kra_details.sendBackComment",
-                      "kra_details_supervisorStatus": "$kra_details.supervisorStatus",
-                      "kra_details_measureOfSuccess": "$kra_details.measureOfSuccess",
-                      "kra_details_unitOfSuccess": "$kra_details.unitOfSuccess",
-                      "kra_details_weightage_id": "$kra_details.weightage_id",
-                      "kra_details_category_id": "$kra_details.category_id",
-                      "kra_details_kra": "$kra_details.kra",
-                      "kra_Details_id": "$kra_details._id"
-                    }
+              //query1
+              KraWorkFlowInfo.aggregate([
+                {
+                  $match: {
+                    emp_id: {
+                      $in: emp_id_collection
+                    },
+                    status: "Initiated"
                   }
-                ]),
+                },
+                {
+                  $lookup: {
+                    from: "kradetails",
+                    localField: "_id",
+                    foreignField: "kraWorkflow_id",
+                    as: "kra_details"
+                  }
+                },
+                {
+                  $unwind: {
+                    path: "$kra_details",
+                    preserveNullAndEmptyArrays: true
+                  }
+                },
+                {
+                  $project: {
+                    kra_batch_id: "$batch_id",
+                    kra_emp_id: "$emp_id",
+                    kra_status: "$status",
+                    kra_details_isDeleted: "$kra_details.isDeleted",
+                    kra_details_sendBackComment: "$kra_details.sendBackComment",
+                    kra_details_supervisorStatus:
+                      "$kra_details.supervisorStatus",
+                    kra_details_measureOfSuccess:
+                      "$kra_details.measureOfSuccess",
+                    kra_details_unitOfSuccess: "$kra_details.unitOfSuccess",
+                    kra_details_weightage_id: "$kra_details.weightage_id",
+                    kra_details_category_id: "$kra_details.category_id",
+                    kra_details_kra: "$kra_details.kra",
+                    kra_Details_id: "$kra_details._id"
+                  }
+                }
+              ]),
               MidTermDetails.aggregate([
                 {
                   $sort: {
@@ -255,8 +278,8 @@ function InitiateMtrProcess(req, res) {
                 {
                   $limit: 1.0
                 }
-              ])]
-            ).then(function (responses, err) {
+              ])
+            ]).then(function(responses, err) {
               if (err) {
                 return res.status(403).json({
                   title: "There was a problem",
@@ -270,11 +293,18 @@ function InitiateMtrProcess(req, res) {
               } else {
                 let mtrDetailsInsertData = [];
                 let kraWorklowResp = responses[0];
-                let mtrDetailsMaxId = responses[1][0] === undefined ? 1 : responses[1][0]._id;
-                kraWorklowResp.forEach((f) => {
-                  f.mtr_batch_id = midTermMasterResult.find(f1 => f1.emp_id === f.kra_emp_id).batch_id;
-                  f.mtr_master_id = midTermMasterResult.find(f1 => f1.emp_id === f.kra_emp_id)._id;
-                  f.emp_supervisor_id = emp_id_array.find(f1 => f1.emp_id === f.kra_emp_id).supervisor_id;
+                let mtrDetailsMaxId =
+                  responses[1][0] === undefined ? 1 : responses[1][0]._id;
+                kraWorklowResp.forEach(f => {
+                  f.mtr_batch_id = midTermMasterResult.find(
+                    f1 => f1.emp_id === f.kra_emp_id
+                  ).batch_id;
+                  f.mtr_master_id = midTermMasterResult.find(
+                    f1 => f1.emp_id === f.kra_emp_id
+                  )._id;
+                  f.emp_supervisor_id = emp_id_array.find(
+                    f1 => f1.emp_id === f.kra_emp_id
+                  ).supervisor_id;
                 });
                 console.log(kraWorklowResp);
                 kraWorklowResp.forEach((f, i) => {
@@ -287,13 +317,19 @@ function InitiateMtrProcess(req, res) {
                       status: "Pending",
                       mtr_batch_id: f.mtr_batch_id,
                       mtr_master_id: f.mtr_master_id,
-                      isDeleted: false
+                      isDeleted: false,
+                      mtr_kra: f.kra_details_kra,
+                      weightage_id: f.kra_details_weightage_id,
+                      unitOfSuccess: f.kra_details_unitOfSuccess,
+                      measureOfSuccess: f.kra_details_measureOfSuccess
                     });
                   } else {
-
                   }
                 });
-                MidTermDetails.insertMany(mtrDetailsInsertData, function (err, midTermDetails) {
+                MidTermDetails.insertMany(mtrDetailsInsertData, function(
+                  err,
+                  midTermDetails
+                ) {
                   if (err) {
                     return res.status(403).json({
                       title: "There was a problem",
@@ -305,13 +341,20 @@ function InitiateMtrProcess(req, res) {
                       }
                     });
                   } else {
-                    AuditTrail.auditTrailEntry(0, "midTermDetails", mtrDetailsInsertData, "user", "midTermDetails", "ADDED");
+                    AuditTrail.auditTrailEntry(
+                      0,
+                      "midTermDetails",
+                      mtrDetailsInsertData,
+                      "user",
+                      "midTermDetails",
+                      "ADDED"
+                    );
                     // sendEmailToAllEmployee(emp_id_array, res);
                     return res.status(200).json({ result: midTermDetails });
                   }
                 });
               }
-            })
+            });
           }
         });
       });
@@ -322,77 +365,77 @@ function GetMtrKraSingleDetails(req, res) {
   let emp_id = parseInt(req.body.emp_id);
   MidTermDetails.aggregate([
     {
-      "$lookup": {
-        "from": "midtermmasters",
-        "localField": "mtr_master_id",
-        "foreignField": "_id",
-        "as": "mtr_master_details"
+      $lookup: {
+        from: "midtermmasters",
+        localField: "mtr_master_id",
+        foreignField: "_id",
+        as: "mtr_master_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$mtr_master_details"
+      $unwind: {
+        path: "$mtr_master_details"
       }
     },
     {
-      "$lookup": {
-        "from": "kradetails",
-        "localField": "kraDetailId",
-        "foreignField": "_id",
-        "as": "kra_details"
+      $lookup: {
+        from: "kradetails",
+        localField: "kraDetailId",
+        foreignField: "_id",
+        as: "kra_details"
       }
     },
     {
-      "$lookup": {
-        "from": "employeedetails",
-        "localField": "mtr_master_details.emp_id",
-        "foreignField": "_id",
-        "as": "emp_details"
+      $lookup: {
+        from: "employeedetails",
+        localField: "mtr_master_details.emp_id",
+        foreignField: "_id",
+        as: "emp_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$emp_details"
+      $unwind: {
+        path: "$emp_details"
       }
     },
     {
-      "$lookup": {
-        "from": "employeedetails",
-        "localField": "supervisor_id",
-        "foreignField": "_id",
-        "as": "emp_supervisor_details"
+      $lookup: {
+        from: "employeedetails",
+        localField: "supervisor_id",
+        foreignField: "_id",
+        as: "emp_supervisor_details"
       }
     },
     {
-      "$unwind": {
-        "path": "$emp_supervisor_details"
+      $unwind: {
+        path: "$emp_supervisor_details"
       }
     },
     {
-      "$project": {
-        "_id": "$_id",
-        "mtr_master_kraWorkflow_id": "$kraWorkflow_id",
-        "mtr_master_kraDetailId": "$kraDetailId",
-        "mtr_master_emp_comment": "$emp_comment",
-        "mtr_master_supervisor_comment": "$supervisor_comment",
-        "mtr_master_supervisor_id": "$supervisor_id",
-        "mtr_batch_id": "$mtr_batch_id",
-        "mtr_master_id": "$mtr_master_id",
-        "mtr_master_status": "$status",
-        "emp_id": "$mtr_master_details.emp_id",
-        "emp_userName": "$emp_details.userName",
-        "emp_full_name": "$emp_details.fullName",
-        "supervisor_userName": "$emp_supervisor_details.userName",
-        "supervisor_full_name": "$emp_supervisor_details.fullName",
-        "kra_details": "$kra_details"
+      $project: {
+        _id: "$_id",
+        mtr_master_kraWorkflow_id: "$kraWorkflow_id",
+        mtr_master_kraDetailId: "$kraDetailId",
+        mtr_master_emp_comment: "$emp_comment",
+        mtr_master_supervisor_comment: "$supervisor_comment",
+        mtr_master_supervisor_id: "$supervisor_id",
+        mtr_batch_id: "$mtr_batch_id",
+        mtr_master_id: "$mtr_master_id",
+        mtr_master_status: "$status",
+        emp_id: "$mtr_master_details.emp_id",
+        emp_userName: "$emp_details.userName",
+        emp_full_name: "$emp_details.fullName",
+        supervisor_userName: "$emp_supervisor_details.userName",
+        supervisor_full_name: "$emp_supervisor_details.fullName",
+        kra_details: "$kra_details"
       }
     },
     {
-      "$match": {
-        "emp_id": emp_id
+      $match: {
+        emp_id: emp_id
       }
     }
-  ]).exec(function (err, data) {
+  ]).exec(function(err, data) {
     if (err) {
       return res.status(403).json({
         title: "There was a problem",
--- models/midterm/midtermdetails.js
+++ models/midterm/midtermdetails.js
@@ -1,42 +1,50 @@
-let mongoose = require('mongoose'),
+let mongoose = require("mongoose"),
   Schema = mongoose.Schema,
-  mongooseUniqueValidator = require('mongoose-unique-validator'),
-  bcrypt = require('bcrypt');
-autoIncrement = require('mongoose-sequence')(mongoose);
+  mongooseUniqueValidator = require("mongoose-unique-validator"),
+  bcrypt = require("bcrypt");
+autoIncrement = require("mongoose-sequence")(mongoose);
 
 let MidTermDetailsSchema = new Schema(
   {
     _id: { type: Number },
-    kraWorkflow_id: { type: Number, ref: 'kraworkflowdetails', required: true },
-    kraDetailId: { type: Number, ref: 'kradetails', required: true },
+    kraWorkflow_id: { type: Number, ref: "kraworkflowdetails", required: true },
+    kraDetailId: { type: Number, ref: "kradetails", default: null },
     emp_comment: { type: String },
     supervisor_comment: { type: String },
     supervisor_id: { type: Number },
     mtr_batch_id: { type: Number, required: true },
     mtr_master_id: { type: Number, required: true },
     status: { type: String },
+    mtr_kra: { type: String, default: null },
+    weightage_id: { type: Number, default: null },
+    unitOfSuccess: { type: String, default: null },
+    measureOfSuccess: { type: String, default: null },
     isDeleted: { type: Boolean, default: false },
     createdBy: { type: Number, default: null },
-    updatedBy: { type: Number, default: null },
+    updatedBy: { type: Number, default: null }
   },
   {
     timestamps: true,
     versionKey: false,
     _id: false
-  });
+  }
+);
 
 // Update the Emp_id Hash user password when registering or when changing password
-MidTermDetailsSchema.pre('save', function (next) {
+MidTermDetailsSchema.pre("save", function(next) {
   var _this = this;
   if (_this.isNew) {
-    //Check the Count of Collection and add 1 to the Count and Assign it to Emp_id 
-    mongoose.model('midtermDetails', MidTermDetailsSchema).find().sort({ _id: -1 }).limit(1)
-      .exec(function (err, doc) {
+    //Check the Count of Collection and add 1 to the Count and Assign it to Emp_id
+    mongoose
+      .model("midtermDetails", MidTermDetailsSchema)
+      .find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .exec(function(err, doc) {
         if (doc.length > 0) {
           _this._id = doc[0]._id + 1;
           next();
-        }
-        else {
+        } else {
           _this._id = 1;
           next();
         }
@@ -46,4 +54,4 @@ MidTermDetailsSchema.pre('save', function (next) {
 
 MidTermDetailsSchema.plugin(mongooseUniqueValidator);
 
-module.exports = mongoose.model('midtermDetails', MidTermDetailsSchema);
+module.exports = mongoose.model("midtermDetails", MidTermDetailsSchema);
